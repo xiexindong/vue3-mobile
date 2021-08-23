c7a648feebba3a6e96cde89b0160ce40a9c2453f
xindong.xie <779788582@qq.com>
2021-08-23 18:35:59 +0800

    搭建初始化项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <router-view v-slot="{Component}">
+      <transition>
+        <component :is="Component"/>
+      </transition>
+    </router-view>    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    
+  },
+  computed: {
+   
+  }
+})
+</script>
