8b47af9cd8bf4e09781f1ddd57db1a58515361f8
xindong.xie <779788582@qq.com>
2021-09-28 19:25:13 +0800

    111

--- src/components/city-picker/index.tsx
+++ src/components/city-picker/index.tsx
@@ -1,5 +1,7 @@
 import { defineComponent,ref } from 'vue';
 import useExpose from '@/composition/use-expose'
+import CITYS from './city.json';
+import { Picker } from 'vant';
 
 export default defineComponent({
     setup(props,context){
@@ -10,13 +12,21 @@ export default defineComponent({
             showPicker.value = true
         }
 
+        function initAddressData(){
+            const copyData = []
+            CITYS.map(n=>{
+                
+            })
+        }
+
         useExpose({ open })
         return()=>(<van-popup 
                 position="bottom"
-                v-model={[showPicker.value]}
+                v-model={[showPicker.value,'show']}
             >
-
-            <div>2222</div>
+                <Picker
+                    columns = {CITYS}
+                />
         </van-popup>)
     }
 })
